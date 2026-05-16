8333b7a891a81724212c9be2d5ed66c68f8d1145
Alexis Pizarro Abarca <alexpizarro3@gmail.com>
2026-05-15 21:55:16 -0600

    Register PWA service worker explicitly in main.jsx to enable Android install prompt

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Register the PWA Service Worker
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
